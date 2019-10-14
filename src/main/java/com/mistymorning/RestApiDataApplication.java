2deb8d5d5d8ef99fb565a4c11523e759fca931e4
John Alexander <johnPersonal@Johns-MBP.broadband>
2019-10-14 07:16:25 +0100

    logging works now

--- src/main/java/com/mistymorning/RestApiDataApplication.java
+++ src/main/java/com/mistymorning/RestApiDataApplication.java
@@ -1,12 +1,17 @@
 package com.mistymorning;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApiDataApplication {
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(RestApiDataApplication.class);
+	
 	public static void main(String[] args) {
+		LOGGER.info("***********Test logging message at startup in "+ RestApiDataApplication.class.getSimpleName());
 		System.setProperty("server.servlet.context-path", "/rest");
 		SpringApplication.run(RestApiDataApplication.class, args);
 	}
